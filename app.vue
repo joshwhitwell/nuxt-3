4569a17480d83584d1ccfb266382f6f34a327333
Josh Whitwell <josh.whitwell@gmail.com>
2023-06-27 20:38:46 -0400

    feat: add overview page

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="max-w-6xl mx-auto px-4 sm:px-6 md:px-8 pt-44">
+    <div class="grid grid-cols-10 gap-x-8">
+      <aside class="col-start-1 col-end-3">
+        <nav>
+          <ul>
+            <li>
+              <NuxtLink to="/">Overview</NuxtLink>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <main class="col-start-3 col-end-10">
+        <NuxtPage />
+      </main >
+    </div>
   </div>
 </template>
+
+<script setup>
+useHead({
+  title: 'Nuxt 3',
+  meta: [
+    { name: 'description', content: 'A demo site for learning about Nuxt 3.' }
+  ],
+  bodyAttrs: {
+    class: 'bg-black text-gray-200 font-sans antialiased'
+  },
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <PageHeader title="Overview" summary="This presentation is intended to provide a general overview of new features available in Nuxt 3 and Vue 3. It is not intended to be an exhaustive survey of all Nuxt 3 and Vue 3 features, for which you should consult the official documentation." icon="both"/>
+  </div>
+</template>
+
+<script setup>
+</script>
